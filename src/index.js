1f39568caacba94442d81bd9f2890ac734ff094f
baxromxoja16 <changeitgo3@gmail.com>
2022-08-01 12:14:08 +0500

    feat: result

--- src/index.js
+++ src/index.js
@@ -39,6 +39,39 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const text = expr
+    let arr = []
+    for(let i = 0; i < (text.length / 10); i++){
+        arr.push(text.slice( i * 10 ,(i * 10) + 10))
+    }
+    const arr2 = []
+    for(let i = 0; i < arr.length; i++){
+        let letter = ''
+        if(arr[i].slice(0, 2) === '*'){
+            letter = ' '
+        }else{
+            for(let j = 0; j < arr[i].length; j++){
+                if(arr[i].slice(j * 2, (j * 2) + 2) === '10'){
+                    letter += '.'
+                }else if(arr[i].slice(j * 2, (j * 2) + 2) === '11'){
+                    letter += '-'
+                }
+            }
+        }
+        arr2.push(letter)
+    }
+    let res = ''
+    arr2.forEach(letter => {
+        for(let key in MORSE_TABLE){
+            if(letter === key){
+                res += MORSE_TABLE[key]
+            }
+        }
+        if(letter === ''){
+            res += ' '.repeat(1)
+        }
+    })
+    return res
 }
 
 module.exports = {
